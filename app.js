81c4b55b9192d23a4e4c41d3b7d4da673e849d20
faanid <fatemeekharazmi@gmail.com>
2026-06-08 12:32:58 -0700

    feat: Refactoring Routes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-express = require("express");
+const express = require("express");
 
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
-app.get("/api/v1/tours/:id", (req, res) => {
+const getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
@@ -21,11 +21,10 @@ app.get("/api/v1/tours/:id", (req, res) => {
       tour,
     },
   });
-});
+};
 
-app.post("/api/v1/tours", (req, res) => {
+const creatTour = (req, res) => {
   // console.log(req.body);
-
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -35,9 +34,8 @@ app.post("/api/v1/tours", (req, res) => {
     status: "success",
     received: req.body,
   });
-});
-
-app.patch("/api/v1/tours/:id", (req, res) => {
+};
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: "fail",
@@ -47,10 +45,44 @@ app.patch("/api/v1/tours/:id", (req, res) => {
   res.status(200).json({
     status: "success",
     data: {
-      tour: "Updated tour here...>",
+      tour: "<Updated tour here...>",
     },
   });
-});
+};
+
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      tours,
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+};
+// app.get("/api/v1/tours", getAllTours);
+// app,get('/api/v1/tours/:id',getTour)
+// app.post("/api/v1/tours", creatTour);
+// app.patch("/api/v1/tours/:id", updateTour);
+// app.delete("/api/v1/tours/:id", deleteTour);
+
+app.route("/api/v1/tours").get(getAllTours).post(creatTour);
+app
+  .route("/api/v1/tours/:id")
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
