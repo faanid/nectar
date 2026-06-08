3bc8a581e2fce13e7f9a083bff6ccea3c7bcac43
faanid <fatemeekharazmi@gmail.com>
2026-06-07 18:06:31 -0700

    handling patch requests

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ app.get("/api/v1/tours/:id", (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
-  // if (id > tours.length) {
-  if (id > tours.length) {
+  if (!tour) {
     return res.status(404).json({
       status: "fail",
       message: "Invalid ID",
@@ -35,6 +34,21 @@ app.post("/api/v1/tours", (req, res) => {
   res.status(201).json({
     status: "success",
     received: req.body,
+  });
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "Updated tour here...>",
+    },
   });
 });
 
