5aaba5d53d17321f5afd39b5a82cead0c5e650e1
faanid <fatemeekharazmi@gmail.com>
2026-06-07 17:53:11 -0700

    feat: responding to url parameters

--- app.js
+++ app.js
@@ -2,15 +2,43 @@ express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "hey", app: "Nectar" });
+// app.use(express.json());
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length) {
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
 });
 
-app.post("/", (req, res) => {
-  res.send("you can post to this endpoint...");
+app.post("/api/v1/tours", (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  res.status(201).json({
+    status: "success",
+    received: req.body,
+  });
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`app running on port &{port}...`);
+  console.log(`app running on port ${port}...`);
 });
