6170b8dbb9c3c6a2b4c23ad3db52e448e79f4887
faanid <fatemeekharazmi@gmail.com>
2026-06-10 12:49:11 -0700

    refactor(mvc): extract controllers and separate app from server startup

---
+++ controllers/tourController.js
@@ -0,0 +1,93 @@
+const fs = require("fs");
+
+const tours = JSON.parse(
+  require("fs").readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
+);
+
+exports.getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
+  res.status(200).json({
+    status: "success",
+
+    requestedAt: req.requestTime,
+
+    results: tours.length,
+
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.creatTour = (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  res.status(201).json({
+    status: "success",
+
+    received: req.body,
+  });
+};
+
+exports.updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+
+    data: {
+      tour: "<Updated tour here...>",
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(204).json({
+    status: "success",
+
+    data: null,
+  });
+};
