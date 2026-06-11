632a4a6e73253f15c3c26fffde85b4c7cce0e8ed
faanid <fatemeekharazmi@gmail.com>
2026-06-11 16:58:03 -0700

    feat: add route param middleware for tour ID validation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,9 +4,19 @@ const tours = JSON.parse(
   require("fs").readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+
+      message: "Invalid ID",
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
-
   res.status(200).json({
     status: "success",
 
@@ -24,14 +34,6 @@ exports.getTour = (req, res) => {
   const id = req.params.id * 1;
 
   const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-
-      message: "Invalid ID",
-    });
-  }
 
   res.status(200).json({
     status: "success",
@@ -59,14 +61,6 @@ exports.creatTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-
-      message: "Invalid ID",
-    });
-  }
-
   res.status(200).json({
     status: "success",
 
@@ -77,14 +71,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-
-      message: "Invalid ID",
-    });
-  }
-
   res.status(204).json({
     status: "success",
 
