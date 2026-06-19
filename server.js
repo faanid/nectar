c7c2d21225bb4b70943bf5c844d87c1d5d8e7880
faanid <fatemeekharazmi@gmail.com>
2026-06-18 17:45:30 -0700

    feat: connect Express app to MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-
 const app = require("./app");
 
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<DATABASE_PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    // .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => console.log("Connected to the database successfully!"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
