c0ae556985d79576d1f73d6a8c705395985a7715
fatemekh <73032767+faanid@users.noreply.github.com>
2026-06-27 20:32:52 +0330

    feat: add pagination for make the api much better

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,6 +24,17 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v')
     } 
 
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page -1 ) * limit;
+
+    //page=3&limit=10, page 1, 11-20, page2, 21-30 page 3
+    query = query.skip(skip).limit(limit);  
+
+    if(req.query.page){
+      const numTours = await Tour.countDocuments();
+      if(skip >= numTours) throw new Error('This page does not exist')
+    }
 
     const tours = await query;
 
