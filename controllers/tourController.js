13640c9cda89417b46454331243803af401b3b25
fatemekh <73032767+faanid@users.noreply.github.com>
2026-07-03 18:23:14 +0330

    feat: aggregation (unwinding and projecting)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -147,3 +147,56 @@ exports.getTourStats = async (req, res) => {
     });
   }
 };
+
+exports.getMonthlyPlan = async(req,res) =>{
+  try{
+    const year = req.params.year *1;
+    const plan = await Tour.aggregate([ 
+      {
+        $unwind:'$startDates'
+      },
+     {
+       $match: {
+        startDates:{
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`)
+        }
+      }
+     },
+     {
+      $group:{
+        _id: {$month: '$startDates'},
+        numTourStart:{$sum:1},
+        tours:{$push: '$name  '}
+      }
+    },
+      {
+        $addField:{month:'$_id'}
+      },
+      {
+        $project:{_id: 0}
+      },
+        {
+          $sort: {numTourStars: -1}
+        },
+     {
+      $limit: 6
+     }
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        stats,
+      },
+    });
+
+  } catch{
+    res.status(200).json({
+      status: "success",
+      data: {
+        stats,
+      },
+    });
+  }
+}
