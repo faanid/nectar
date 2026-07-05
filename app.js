10849242de00f86a4e37d23912c88cbfd686df98
fatemekh <73032767+faanid@users.noreply.github.com>
2026-07-05 17:28:52 +0330

    feat: implementing a global error handling

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const morgan = require("morgan");
 const swaggerjsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoute");
 
@@ -17,17 +20,18 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/dev-data/public`));
 
 app.use((req, res, next) => {
-  console.log("middlewawre");
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all('*', (req,res,next)=>{
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`))
+});
+
+app.use(globalErrorHandler);
 
 const options = {
   definition: {
