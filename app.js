04b671acebafc5f7ee4500aae0566f5e9241f089
faanid <fatemeekharazmi@gmail.com>
2026-08-14 09:43:03 -0700

    feat: add xss, mongo sanitization and hpp protectio

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const swaggerjsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -30,6 +33,26 @@ app.use("/api", limiter);
 
 //body parser, reading data from into req.body
 app.use(express.json({ limit: "10kb" }));
+
+//data sanitization against nosql query injection
+app.use(mongoSanitize());
+
+//data sanitization for xss
+app.use(xss());
+
+//prevent parameter pullution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "difficulty",
+      "ratingsAverage",
+      "price",
+      "maxGroupSize",
+    ],
+  }),
+);
 
 app.use(express.static(`${__dirname}/dev-data/public`));
 
