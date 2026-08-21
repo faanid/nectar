6e3b3bf578f4bb5026332be3bf0cf97ce274a19a
fatemekh <73032767+faanid@users.noreply.github.com>
2026-08-21 11:05:51 +0330

    feat: building handler factory function delete

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,7 @@
 const Review = require("../models/reviewModel");
 const catchAsync = require("../utils/catchAsync");
+const factory = require("./handlerFactory");
+
 
 exports.getAllReviews = catchAsync(async (req, res) => {
   let filter ={}
@@ -29,3 +31,5 @@ exports.createReview = catchAsync(async (req, res) => {
     },
   });
 });
+
+exports.deleteReview = factory.deleteOne(Review);
