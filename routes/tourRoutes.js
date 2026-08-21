805a41037807a15a977ba9e89a7b8dae47a5689b
fatemekh <73032767+faanid@users.noreply.github.com>
2026-08-21 10:51:47 +0330

    feat: adding a nested route using express and merge params

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -62,8 +62,11 @@
 const express = require("express");
 const tourController = require("../controllers/tourController");
 const authController = require("../controllers/authController");
+const reviewRouter = require("./../routes/reviewRoutes");
 
 const router = express.Router();
+
+router.use('/:tourId/reviews', reviewRouter);
 
 // router.param("id", tourController.checkID);
 router
@@ -86,5 +89,13 @@ router
     authController.restrictTo("admin", "lead-guide"),
     tourController.deleteTour,
   );
+
+// router.route('/:tourId/reviews')
+// .post(
+// authController.protect,
+// authController.restrictTo('user'),
+// reviewController.createReview
+// );
+
 
 module.exports = router;
